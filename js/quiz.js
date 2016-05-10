ef1ab95d1c057bd7dbb3cc2315a840552dd30d01
Bernard Anderson <bernard.anderson@gmail.com>
2016-05-10 10:23:25 -0500

    quiz.js converted to ES6

--- js/quiz.js
+++ js/quiz.js
@@ -1,24 +1,24 @@
 "use strict";
 
-$(document).ready(function() {
+$(document).ready(() => {
 
 // This loads and parses the JSON and calls the methods to build all the constructor functions for the robot types,
 //  models, weapons and modificiations.  It's found in the "robotJsonAccess.js" file.
   RobotWars.MainLoadBuildPromise();
 
   // This watches for click events in the entire input area when the robot players are being built
-  $("#main-input-holder").click( function(event) {
+  $("#main-input-holder").click( (event) => {
 
     RobotWars.displayPlayerBuildButtons(event.target);
 
   });
 
   // This watches for mouseovers in the entire input area when the robot players are being built
-  $("#main-input-holder").mouseover(function(event) {
+  $("#main-input-holder").mouseover( (event) => {
 
     RobotWars.displayItemStatistics(event.target);
 
-    }).mouseleave( function() {
+    }).mouseleave( () => {
 
       $("#output-panel").html("");
 
